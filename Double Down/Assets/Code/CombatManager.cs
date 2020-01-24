83466c71ce1a427106f82220d6d909a31c2830a4
Joe <zaleacon7@gmail.com>
2020-01-24 00:56:15 -0500

    Began working on scene, added stats, and added turn order.

---
+++ Double Down/Assets/Code/CombatManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    public GameObject playerChars;
+    public GameObject enemyChars;
+    [HideInInspector] public List<GameObject> playerCharsList = null;
+    [HideInInspector] public List<GameObject> enemyCharsList = null;
+    [HideInInspector] public List<GameObject> combatChars = null;
+
+    private void Start()
+    {
+        for (int i = 0; i < playerChars.transform.childCount; ++i)
+        {
+            playerCharsList.Add(playerChars.transform.GetChild(i).gameObject);
+            combatChars.Add(playerChars.transform.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < enemyChars.transform.childCount; ++i)
+        {
+            enemyCharsList.Add(enemyChars.transform.GetChild(i).gameObject);
+            combatChars.Add(enemyChars.transform.GetChild(i).gameObject);
+        }
+
+        SetTurnOrder();
+    }
+
+    private void StartTurn()
+    {
+
+    }
+
+    // Sorts the character order for each turn
+    // Can be used to re-sort the order mid-turn
+    private void SetTurnOrder()
+    {
+        bool sorted = false;
+        bool sorting = true;
+        GameObject temp = null;
+        
+        while (sorting)
+        {
+            for (int i = 0; i < combatChars.Count; ++i)
+            {
+                if (i != 0 && combatChars[i].GetComponent<Stats>().Speed() > combatChars[i - 1].GetComponent<Stats>().Speed())
+                {
+                    sorted = false;
+
+                    temp = combatChars[i];
+                    combatChars[i] = combatChars[i - 1];
+                    combatChars[i - 1] = temp;
+                }
+            }
+
+            if (!sorted)
+                sorted = true;
+
+            else
+                sorting = false;
+        }
+    }
+
+    private void DealDamage()
+    {
+        //target.ReduceHP(DamageFormula(chara, target, 1));
+        //Debug.Log(target.HP());
+    }
+
+    private int DamageFormula(Stats a, Stats t, float mod)
+    {
+        int i = (int)(((a.Attack() * a.Attack()) / t.Defense()) * mod * Random.Range(0.8f, 1.0f));
+        return i;
+    }
+}
---
+++ Double Down/Assets/Code/Stats.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    [Header("Main Stats")]
+    [SerializeField] private int maxHP = 100;
+    private int currentHP = 100;
+    [SerializeField] private int maxTP = 25;
+    private int currentTP = 25;
+
+    [SerializeField] private int atk = 10;
+    [SerializeField] private int def = 10;
+    [SerializeField] private int spd = 10;
+    
+    // Stat Modifiers
+    private float atkMod = 1;
+    private float defMod = 1;
+    private float spdMod = 1;
+
+    // Modifier Turns
+    private int atkModTurns = 0;
+    private int defModTurns = 0;
+    private float spdModTurns = 0;
+
+    [Header("Level Gains")]
+    [SerializeField] private int HPGain = 25;
+    [SerializeField] private int TPGain = 25;
+
+    [SerializeField] private int atkGain = 3;
+    [SerializeField] private int defGain = 3;
+    [SerializeField] private int spdGain = 3;
+
+    [Header("Level and Experience")]
+    [SerializeField] private int level = 1;
+    [SerializeField] private int exp = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReduceHP(int i)
+    {
+        currentHP -= i;
+    }
+
+    // Returns the character's current HP
+    public int HP()
+    {
+        return currentHP;
+    }
+
+    // Returns the character's current TP
+    public int TP()
+    {
+        return currentTP;
+    }
+
+    // Returns the character's attack stat
+    public int Attack()
+    {
+        int i = (int)(atk * atkMod);
+        if (i <= 0)
+            i = 1;
+        return i;
+    }
+
+    // Returns the character's defense stat
+    public int Defense()
+    {
+        int i = (int)(def * defMod);
+        if (i <= 0)
+            i = 1;
+        return i;
+    }
+
+    // Returns the character's speed stat
+    public int Speed()
+    {
+        int i = (int)(spd * spdMod);
+        if (i <= 0)
+            i = 1;
+        return i;
+    }
+
+    // Ticks down the modifier turn count at the end of each turn
+    public void TickModifierChanges()
+    {
+        // Ticks down attack changes
+        if (atkModTurns > 0)
+            atkModTurns--;
+        else
+            atkMod = 1;
+
+        // Ticks down defense changes
+        if (defModTurns > 0)
+            defModTurns--;
+        else
+            defMod = 1;
+
+        // Ticks down speed changes
+        if (spdModTurns > 0)
+            spdModTurns--;
+        else
+            spdMod = 1;
+    }
+}
