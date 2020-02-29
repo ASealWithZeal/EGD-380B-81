70fc4eb83d6a0ecace3e00aa5d8794e339b54190
Joe <zaleacon7@gmail.com>
2020-02-29 02:16:38 -0500

    Began implementing the "hub" area and movement.

---
+++ Double Down/Assets/MovementManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovementArrows
+{
+    Up = KeyCode.W | KeyCode.UpArrow,
+    Down = KeyCode.S | KeyCode.DownArrow,
+    Left = KeyCode.A | KeyCode.LeftArrow,
+    Right = KeyCode.D | KeyCode.RightArrow
+}
+
+public class MovementManager : MonoBehaviour
+{
+    public List<CharData> playerChars;
+    private bool canMoveChars = true;
+    private Vector3 movement = new Vector3();
+
+    public float moveDistance = 0.1f;
+    private Vector3[] moves = new Vector3[4]
+    {
+        new Vector3(0, 0, 1),
+        new Vector3(0, 0, -1),
+        new Vector3(1, 0, 0),
+        new Vector3(-1, 0, 0),
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (canMoveChars)
+            MoveChars();
+    }
+
+    private void MoveChars()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Physics.gravity.y * Time.fixedDeltaTime * moveDistance, Input.GetAxisRaw("Vertical"));
+        playerChars[0].gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(input * moveDistance * Time.fixedDeltaTime));
+    }
+}
